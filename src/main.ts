2db761b3b512313cb37e4abd4be6f5020efc1cd0
SuganyaRathinam <119413442+SuganyaRathinam@users.noreply.github.com>
2024-02-19 12:00:48 +0530

    Added sample

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
+import { registerLicense } from '@syncfusion/ej2-base';
+
+registerLicense('ORg4AjUWIQA/Gnt2UVhiQldPdUBDX3xLflFyVWVTf1Z6cVdWESFaRnZdRl1hSHtTckBnXXhbcnNR');
+
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
